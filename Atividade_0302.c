58f2b578c9a7a5f1c960b8203325e1783e846191
Tácio Patez <taciopatez@hotmail.com>
2025-02-10 00:55:25 -0300

    Final Commit

--- Atividade_0302.c
+++ Atividade_0302.c
@@ -3,6 +3,9 @@
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
+#include "hardware/pio.h"
+#include "ws2812.pio.h"
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -18,6 +21,10 @@
 #define BAUD_RATE 115200 // Define a taxa de transmissão
 #define UART_TX_PIN 0 // Pino GPIO usado para TX
 #define UART_RX_PIN 1 // Pino GPIO usado para RX
+
+#define NUM_PIXELS 25
+#define IS_RGBW false
+#define WS2812_PIN 7
 
 // Armazena o tempo do último evento (em microssegundos)
 static volatile uint32_t last_time = 0; 
@@ -37,6 +44,132 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
     }
 }
 
+// Buffer para armazenar quais LEDs estão ligados matriz 5x5
+bool led_buffer_1[NUM_PIXELS] = {
+  0, 1, 0, 0, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 0, 0, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 0, 0, 0
+};
+
+bool led_buffer_2[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 0, 0, 
+  0, 1, 1, 1, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_3[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_4[NUM_PIXELS] = {
+  0, 1, 0, 0, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 0, 1, 0
+};
+
+bool led_buffer_5[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 0, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_6[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 0, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_7[NUM_PIXELS] = {
+  0, 1, 0, 0, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 0, 0, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_8[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_9[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 0, 0, 1, 0, 
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_0[NUM_PIXELS] = {
+  0, 1, 1, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 0, 1, 0, 
+  0, 1, 1, 1, 0
+};
+
+bool led_buffer_nothing[NUM_PIXELS] = {
+  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0
+};
+
+// Array de ponteiros para os buffers
+bool* led_buffers[10] = {
+  led_buffer_0, led_buffer_1, led_buffer_2, led_buffer_3, led_buffer_4,
+  led_buffer_5, led_buffer_6, led_buffer_7, led_buffer_8, led_buffer_9
+};
+
+static inline void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+
+void set_one_led(char c) {
+  uint8_t number = c - '0';
+  // Define a cor
+  uint32_t color = urgb_u32(255, 0, 0);
+
+  bool* buffer = c == 'n' ? led_buffer_nothing : led_buffers[number % 10];
+  // Define todos os LEDs com a cor especificada
+  for (int i = 0; i < NUM_PIXELS; i++)
+  {
+      if (buffer[i])
+      {
+        uart_puts(UART_ID, "Teste\r\n");
+          put_pixel(color); // Liga o LED com um no buffer
+      }
+      else
+      {
+          put_pixel(0);  // Desliga os LEDs com zero no buffer
+      }
+  }
+}
 
 int main() {
   // Inicializa comunicação USB CDC para monitor serial
@@ -87,6 +220,13 @@ int main() {
   gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
   gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
+  //inicializacao pio
+  PIO pio = pio0;
+  int sm = 0;
+  uint offset = pio_add_program(pio, &ws2812_program);
+
+  ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+
   while (true) {
     if (uart_is_readable(UART_ID)) {
       // Lê um caractere da UART
@@ -94,7 +234,11 @@ int main() {
       ssd1306_fill(&ssd, false);
       ssd1306_draw_char(&ssd, c, 30, 30);    
       ssd1306_send_data(&ssd); // Atualiza o display
-      
+      if (c >= '0' && c <= '9'){
+        set_one_led(c);
+      } else {
+        set_one_led('n');
+      }
       // Envia de volta o caractere lido (eco)
       uart_putc(UART_ID, c);
       // Envia uma mensagem adicional para cada caractere recebido
